3668c2b6ff5fb3199307eeac60a8a2409eb43a6a
Omar Qunsul <omar.qunsul@gmail.com>
2020-05-03 11:59:05 +0200

    report

--- engine.py
+++ engine.py
@@ -3,8 +3,11 @@ import re
 from stockfish import Stockfish
 from flask import Flask
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
+
+LEVEL = 15
 
 def eval(self):
     self.stockfish.stdin.write(f"eval\n")
@@ -18,10 +21,33 @@ def eval(self):
 
 Stockfish.eval = eval
 
+@app.route('/engine/report', methods=['POST', 'GET'])
+def report():
+    global LEVEL
+    stockfish = Stockfish('/usr/games/stockfish')
+    stockfish.set_skill_level(LEVEL)
+    moves = request.args.get('moves', '').split(',')
+    ret = []
+    for i in range(len(moves)):
+        move = moves[i]
+        stockfish.set_position(moves[0:i+1])
+        move_eval = stockfish.eval()
+        best_move = stockfish.get_best_move()
+        item = { "index": i, "move": move, "eval": move_eval, "best_move": best_move }
+        if i > 0 and move == ret[-1]["best_move"]:
+            item["match"] = True
+        else:
+            item["match"] = False
+        ret.append(item)
+
+    return jsonify({"moves": ret})
+
+
 @app.route('/engine/position', methods=['POST', 'GET'])
 def position():
+    global LEVEL
     stockfish = Stockfish('/usr/games/stockfish')
-    stockfish.set_skill_level(15)
+    stockfish.set_skill_level(LEVEL)
     moves = request.args.get('moves', '').split(',')
     index = request.args.get('index', '')
     if index:
